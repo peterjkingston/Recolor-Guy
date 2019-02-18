7a1c60ffea738688f06002d8a2e818e3e6134e68
Peter Kingston <46848056+peterjkingston@users.noreply.github.com>
2019-02-18 02:16:52 -0800

    Create Numbers.cs

---
+++ Numbers.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recolor_Guy
+{
+    public static class Numbers
+    {
+        public static float Max(params float[] numbers)
+        {
+            return numbers.Max();
+        }
+
+        public static float Min(params float[] numbers)
+        {
+            return numbers.Min();
+        }
+
+        public static byte LargestOf(byte[] colorBytes)
+        {
+            return colorBytes.Max();
+        }
+
+        public static byte MiddleOf(byte[] bytes)
+        {
+  
+            byte valR = bytes[2];
+            byte valG = bytes[1];
+            byte valB = bytes[0];
+
+            if ((valR <= valG && valR >= valB) || (valR <= valB && valR >= valG)) { return valR; }
+            if ((valG <= valR && valG >= valB) || (valG <= valB && valG >= valR)) { return valG; }
+            if ((valB <= valG && valB >= valR) || (valB <= valR && valB >= valG)) { return valB; }
+            return 0;
+        }
+
+        public static byte SmallestOf(byte[] bytes)
+        {
+            return bytes.Min();
+        }
+    }
+}
