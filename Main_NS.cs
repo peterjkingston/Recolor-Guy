4980ba07f864da9aa219b17ed4279e994233ab9b
Peter Kingston <46848056+peterjkingston@users.noreply.github.com>
2019-02-18 02:19:52 -0800

    Create Main_NS.cs

---
+++ Main_NS.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recolor_Guy
+{
+    public enum Spectrum
+    {
+        Red = -15, Orange = 15, Yellow = 45, Lime = 75, Green = 105, SeaGreen = 135, Aqua = 165, Denim = 195, Blue = 225, Magenta = 255, Violet = 285, Rose = 315, Null = 345, White = 375,
+        Black = 405
+    }
+}
